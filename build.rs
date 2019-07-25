7de342569521ef6a76a43b3a993cab0537767e09
Kornel <kornel@geekhood.net>
2019-07-25 16:33:06 +0100

    Include headers as a fallback always. Build as a fallback always

--- build.rs
+++ build.rs
@@ -17,7 +17,9 @@ fn main() {
     }
 
     if !try_libpng_config(wants_static, std_zlib) {
-        try_pkgconfig(wants_static);
+        if !try_pkgconfig(wants_static) {
+            build_static(std_zlib);
+        }
     }
 }
 
@@ -40,9 +42,14 @@ fn try_libpng_config(wants_static: bool, std_zlib: bool) -> bool {
     }
 
     if let Some(libdir) = libpng_config(wants_static, "--libdir") {
-        let libdir = libdir.trim_end();
-        println!("cargo:rustc-link-search=native={}", libdir);
-        println!("cargo:root={}", libdir);
+        if let Some(args) = libpng_config(wants_static, "--libs") {
+            let libdir = libdir.trim_end();
+            println!("cargo:rustc-link-search=native={}", libdir);
+            println!("cargo:root={}", libdir);
+            libs_from_args(&args, wants_static, std_zlib);
+        } else {
+            return false;
+        }
     } else {
         return false;
     }
@@ -50,14 +57,16 @@ fn try_libpng_config(wants_static: bool, std_zlib: bool) -> bool {
     if let Some(iopts) = libpng_config(wants_static, "--I_opts") {
         let dirs: Vec<_> = iopts.split(" -I").map(|opt| if opt.starts_with("-I") {&opt[2..]} else {opt}).collect();
         println!("cargo:include={}", env::join_paths(dirs).unwrap().to_string_lossy());
-    }
-
-    if let Some(args) = libpng_config(wants_static, "--libs") {
-        libs_from_args(&args, wants_static, std_zlib);
     } else {
-        return false;
+        fallback_include_path();
     }
     true
+}
+
+fn fallback_include_path() {
+    // fallback to bundled headers. It will probably cause problems on version mismatch,
+    // but brokenness of libpng-config can't be helped
+    println!("cargo:include={}", dunce::canonicalize("vendor").unwrap().display());
 }
 
 fn libs_from_args(libs: &str, wants_static: bool, std_zlib: bool) {
@@ -102,6 +111,8 @@ fn try_pkgconfig(wants_static: bool) -> bool {
     if let Ok(lib) = pkg.probe("libpng") {
         if let Some(path) = lib.include_paths.get(0) {
             println!("cargo:include={}", path.display());
+        } else {
+            fallback_include_path();
         }
         if let Some(path) = lib.link_paths.get(0) {
             println!("cargo:root={}", path.display());
