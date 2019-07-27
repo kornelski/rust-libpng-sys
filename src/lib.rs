7271cc9956d7dea01b84bf4bfa2c2e52d021e068
Kornel <kornel@geekhood.net>
2019-07-27 14:41:14 +0100

    Tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,13 @@ extern crate libz_sys;
 pub mod ffi;
 
 #[test]
+fn links() {
+    unsafe {
+        assert!(ffi::png_access_version_number() > 10000);
+    }
+}
+
+#[test]
 fn z() {
     extern "C" {
         fn deflateEnd(x: *mut u8) -> std::os::raw::c_int;
@@ -21,12 +28,13 @@ fn z() {
 }
 
 #[test]
-fn links() {
+fn creates() {
     use std::ptr::null_mut;
     unsafe {
-        assert!(ffi::png_access_version_number() > 10000);
+        assert!(ffi::png_access_version_number() > 10600);
 
-        let res = ffi::png_create_read_struct_2("testing".as_ptr() as *const _, null_mut(), None, None, null_mut(), None, None);
-        assert!(res.is_null());
+        let mut res = ffi::png_create_read_struct_2(ffi::PNG_LIBPNG_VER_STRING, null_mut(), None, None, null_mut(), None, None);
+        assert!(!res.is_null());
+        ffi::png_destroy_read_struct(&mut res, null_mut(), null_mut());
     }
 }
